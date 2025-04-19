f92aba623697f633a05bf3330a756529f72954ff
Harshith <harshith140724@gmail.com>
2025-04-20 01:33:49 +0530

    fix: removed accidental git command from <div> tag

--- components/ui/alert.tsx
+++ components/ui/alert.tsx
@@ -23,7 +23,7 @@ const Alert = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof alertVariants>
 >(({ className, variant, ...props }, ref) => (
-  <divgit branch -M main
+  <div
     ref={ref}
     role="alert"
     className={cn(alertVariants({ variant }), className)}
