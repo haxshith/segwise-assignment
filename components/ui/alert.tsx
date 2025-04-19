fd195c4687bc191e5425437d80c227e8d662566d
Harshith <harshith140724@gmail.com>
2025-04-20 00:53:47 +0530

    Initial commit

--- components/ui/alert.tsx
+++ components/ui/alert.tsx
@@ -23,7 +23,7 @@ const Alert = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof alertVariants>
 >(({ className, variant, ...props }, ref) => (
-  <div
+  <divgit branch -M main
     ref={ref}
     role="alert"
     className={cn(alertVariants({ variant }), className)}
